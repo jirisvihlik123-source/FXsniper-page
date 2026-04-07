a2f5352a710ad4bf507f5c1999ad673226b09659
FXjirka <jirisvihlik123@gmail.com>
2026-04-07 11:53:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+function setLang(lang) {
 
+    if (lang === "cz") {
+        document.getElementById("title").innerText = "Vítej na naší stránce";
+        document.getElementById("btn1").innerText = "Start";
+        document.getElementById("btn2").innerText = "Příběh";
+        document.getElementById("btn3").innerText = "Kontakt";
+    }
+
+    if (lang === "en") {
+        document.getElementById("title").innerText = "Welcome to our page";
+        document.getElementById("btn1").innerText = "Start";
+        document.getElementById("btn2").innerText = "Story";
+        document.getElementById("btn3").innerText = "Contact";
+    }
+
+}
